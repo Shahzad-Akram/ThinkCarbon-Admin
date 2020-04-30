da5e74e811df6c3e9be95e84bddb5fd44e9fb4fb
OmerFarooq <comingbacktolyf@gmail.com>
2020-04-30 20:11:10 +0500

    Refactored Coupon List

--- src/views/viewCoupons.js
+++ src/views/viewCoupons.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
 import { useCoupons } from '../hooks/useCoupons';
-import { Coupon } from '../components/coupon';
 import { useIsAdmin } from '../hooks/useIsAdmin';
 import { setAuthorizationToken } from '../helpers/utils';
 import { Loader } from '../components/spinner';
 import NotFound from '../components/NotFound';
+import Header from '../components/header';
+import CouponList from '../components/coupon/couponList';
 
 export const ViewCoupon = () => {
   setAuthorizationToken();
@@ -19,15 +19,9 @@ export const ViewCoupon = () => {
       ) : (
         <div className='col-md-12 mx-auto  py-4'>
           <div className='row'>
-            <h3 className='border-top border-bottom font-weight-bold py-2 w-100 text-info mb-2'>
-              Coupons
-            </h3>
+            <Header heading='Coupons' item={coupons} />
             {coupons.length > 0 ? (
-              <>
-                {coupons.map((coupon) => (
-                  <Coupon key={coupon._id} coupon={coupon} />
-                ))}
-              </>
+              <CouponList coupons={coupons} />
             ) : (
               <NotFound message='No Active Coupons.' />
             )}

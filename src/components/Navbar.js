dd630b93bb354c9e950f9a83285a204af3a6b687
OmerFarooq <comingbacktolyf@gmail.com>
2020-05-17 00:01:10 +0500

    Added Logo To Sidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,9 @@ import Logo from '../assets/images/logo-white.png';
 import { NavItem, NavLink } from 'reactstrap';
 import { NavLink as RRNavLink } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
-import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar';
+import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import Toggle from 'react-toggle';
+import FeatherIcon from 'feather-icons-react';
 
 const Appbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -34,51 +35,61 @@ const Appbar = () => {
                 tag={RRNavLink}
                 to='/coupons'
               >
-                <img src={Logo} alt='Nadaasi Logo' width='90px' />{' '}
+                <img
+                  className='ml-2'
+                  src={Logo}
+                  alt='Nadaasi Logo'
+                  width='90px'
+                />{' '}
                 <span className='text-white ml-1'>Admin</span>
               </NavLink>
             </MenuItem>
-            <MenuItem>
-              <NavLink
-                exact={true}
-                activeClassName='text-light font-weight-bold '
-                tag={RRNavLink}
-                to='/coupons'
-              >
-                Coupons
-              </NavLink>
-            </MenuItem>
-            <MenuItem>
-              <NavLink
-                exact={true}
-                activeClassName='text-light font-weight-bold '
-                tag={RRNavLink}
-                to='/add-coupon'
-              >
-                Add Coupons
-              </NavLink>
-            </MenuItem>
-            <MenuItem>
-              <NavLink
-                exact={true}
-                activeClassName='text-light font-weight-bold '
-                tag={RRNavLink}
-                to='/upload-product'
-              >
-                Add Product
-              </NavLink>
-            </MenuItem>
-            <MenuItem>
-              <NavLink
-                exact={true}
-                activeClassName='text-light font-weight-bold '
-                tag={RRNavLink}
-                to='/products'
-              >
-                Products
-              </NavLink>
-            </MenuItem>
-            <MenuItem>
+            <SubMenu title='Coupons' icon={<FeatherIcon icon='gift' />}>
+              <MenuItem>
+                {' '}
+                <NavLink
+                  exact={true}
+                  activeClassName='text-light font-weight-bold '
+                  tag={RRNavLink}
+                  to='/coupons'
+                >
+                  Coupons
+                </NavLink>
+              </MenuItem>
+              <MenuItem>
+                <NavLink
+                  exact={true}
+                  activeClassName='text-light font-weight-bold '
+                  tag={RRNavLink}
+                  to='/add-coupon'
+                >
+                  Add Coupons
+                </NavLink>
+              </MenuItem>
+            </SubMenu>
+            <SubMenu title='Products' icon={<FeatherIcon icon='package' />}>
+              <MenuItem>
+                <NavLink
+                  exact={true}
+                  activeClassName='text-light font-weight-bold '
+                  tag={RRNavLink}
+                  to='/upload-product'
+                >
+                  Add Product
+                </NavLink>
+              </MenuItem>
+              <MenuItem>
+                <NavLink
+                  exact={true}
+                  activeClassName='text-light font-weight-bold '
+                  tag={RRNavLink}
+                  to='/products'
+                >
+                  Products
+                </NavLink>
+              </MenuItem>
+            </SubMenu>
+            <MenuItem icon={<FeatherIcon icon='truck' />}>
               <NavLink
                 exact={true}
                 activeClassName='text-light font-weight-bold '
@@ -88,7 +99,7 @@ const Appbar = () => {
                 Orders
               </NavLink>
             </MenuItem>
-            <MenuItem>
+            <MenuItem icon={<FeatherIcon icon='clipboard' />}>
               <NavLink
                 exact={true}
                 activeClassName='text-light font-weight-bold '
@@ -98,7 +109,7 @@ const Appbar = () => {
                 Feedback
               </NavLink>
             </MenuItem>
-            <MenuItem>
+            <MenuItem icon={<FeatherIcon icon='log-out' />}>
               <NavItem>
                 <NavLink
                   onClick={() => {

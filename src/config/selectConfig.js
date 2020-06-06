4aa53a03ba09f0aa185b5b4855748142ef8b9154
OmerFarooq <comingbacktolyf@gmail.com>
2020-06-06 17:31:23 +0500

    Added Dress Types

--- src/config/selectConfig.js
+++ src/config/selectConfig.js
@@ -35,6 +35,35 @@ export const OPTIONS = {
       value: 'evening',
       label: 'Evening',
     },
+    {
+      value: 'a-line',
+      label: 'A-Line',
+    },
+    {
+      value: 'a-line-princess',
+      label: 'A-Line / Princess',
+    },
+    {
+      value: 'modified-a-line',
+      label: 'Modified A-Line',
+    },
+    {
+      value: 'sheath',
+      label: 'Sheath',
+    },
+
+    {
+      value: 'empire-waist',
+      label: 'Empire-Waist',
+    },
+    {
+      value: 'mermaid',
+      label: 'Mermaid',
+    },
+    {
+      value: 'trumpet',
+      label: 'Trumpet',
+    },
   ],
   dressColorOptions: [
     {
